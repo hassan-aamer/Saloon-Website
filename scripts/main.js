7b6e20e67da06100b130ffad347e7134cb5f61e2
Hassan ツ <130797396+hassan-aamer@users.noreply.github.com>
2023-09-08 18:34:03 +0300

    Create Pages About and Services and Testimonial

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,16 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () =>{
     menu.classList.toggle("move")
 }
+//Swiper
+var swiper = new Swiper(".services-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 50000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
